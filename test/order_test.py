e4a181f7d0e32de14c2db85db47ab00ff0f48b9f
Hazel <Hazel@MacNikPro.home>
2023-10-05 10:22:59 +0100

    test for order total

--- test/order_test.py
+++ test/order_test.py
@@ -10,3 +10,17 @@ def test_add_item_to_order():
         order.add_item("Cafe Latte", 2)
 
         assert order.get_item_quantity("Cafe Latte") == 2
+
+def test_order_total():
+        menu = ProductList()
+        menu.add_product("Cafe Latte", 3.50)
+        menu.add_product("Espresso", 2.00)
+
+        order = Order(menu)
+        order.add_item("Cafe Latte", 2)
+        order.add_item("Espresso", 3)
+
+        total = order.calculate_total()
+        assert total == (3.50 * 2) + (2.00 * 3)
+
+
